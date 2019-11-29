df31ec71f13b68ebca74d2fae9f64506812463f0
Aditya Vaishnav <avaishnav@techracers.io>
2019-11-29 22:30:07 +0530

    Initialized all routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,21 @@
-import initBlockRoutes from './blocks.routes'
-import initStatRoutes from './stats.routes'
-import initTransactionRoutes from './transactions.routes'
+import initBlockRoutes from './block.routes';
+import initBlocksRoutes from './blocks.routes';
+import initDocsRoutes from './docs.routes';
+import initStatsRoutes from './stats.routes';
+import initTransactionRoutes from './transaction.routes';
+import initTransactionsRoutes from './transactions.routes';
 
-function initRoutes (app) {
-  app.use('/blocks', initBlockRoutes())
-  app.use('/stats', initStatRoutes())
-  app.use('/transactions', initTransactionRoutes())
-}
+/**
+ * @function initRoutes To initialize routes
+ * @returns {undefined} Adds routes in express application
+ */
+const initRoutes = app => {
+  app.use('/block', initBlockRoutes());
+  app.use('/blocks', initBlocksRoutes());
+  app.use('/docs', initDocsRoutes());
+  app.use('/stats', initStatsRoutes());
+  app.use('/transaction', initTransactionRoutes());
+  app.use('/transactions', initTransactionsRoutes());
+};
 
-export default initRoutes
+export default initRoutes;
