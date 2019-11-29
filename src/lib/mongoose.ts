9042116b0e2718cd0a010d30ad125ae536ca8a84
Aditya Vaishnav <avaishnav@techracers.io>
2019-11-29 23:57:36 +0530

    Removed depricated warnings from mongo

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -4,8 +4,11 @@ import config from 'config';
 const MONGO_URL = config.get('mongodb.url');
 const MONGO_POOL_SIZE = config.get('mongodb.pool_size');
 
-export async function connect() {
-  await mongoose.connect(MONGO_URL, { useNewUrlParser: true, poolSize: MONGO_POOL_SIZE });
+export async function connect () {
+  await mongoose.set('useCreateIndex', true)
+  await mongoose.set('useNewUrlParser', true);
+  await mongoose.set('useUnifiedTopology', true);
+  await mongoose.connect(MONGO_URL, { useNewUrlParser: true, poolSize: MONGO_POOL_SIZE })
 }
 
 export async function getMongoConnection() {
