1c19611245a134d5f3c59487ff68f1c4b8aa0063
Aditya Vaishnav <avaishnav@techracers.io>
2019-11-29 22:29:16 +0530

    Added block routes

--- src/app/routes/blocks.routes.ts
+++ src/app/routes/blocks.routes.ts
@@ -1,14 +1,17 @@
-import express from 'express'
+import express from 'express';
+import BlockController from '../controllers/blocks.controller';
 
-import BlockController from '../controllers/blocks.controller'
+/**
+ * @function initBlocksRoutes To initialize '/blocks' routes
+ * @returns {Router} Returns blocks router
+ */
+const initBlocksRoutes = () => {
+  const BlocksRouter = express.Router();
 
-function initBlockRoutes () {
-  const BlockRouter = express.Router()
+  BlocksRouter.get('/', BlockController.getLatestBlocks);
+  BlocksRouter.get('/search', BlockController.searchBlocks);
 
-  BlockRouter.get('/', BlockController.list)
-  BlockRouter.get('/:blockId', BlockController.get)
+  return BlocksRouter;
+};
 
-  return BlockRouter
-}
-
-export default initBlockRoutes
+export default initBlocksRoutes;
